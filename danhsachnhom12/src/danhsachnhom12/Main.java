3ae318e069aa25b2510e71e3cb386af744ce743a
vanbinh628 <vanbinh628@gmail.com>
2016-09-26 14:20:47 +0700

    Danh Sach Nhom 12

---
+++ danhsachnhom12/src/danhsachnhom12/Main.java
@@ -0,0 +1,14 @@
+package danhsachnhom12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Danh Sach Nhom 12:");
+		System.out.print("Nguyen Van Binh");
+		System.out.print("Bui Khac Trong");
+		System.out.print("Luu Dinh Mac");
+		System.out.print("Le Van Liem");
+	}
+
+}
